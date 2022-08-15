f270fcfd846a0003d8d42ab5ad9ad80a438c94f1
Arch0wl <annstarovoitov@gmail.com>
2022-08-15 19:04:40 -0400

    practice

--- index.py
+++ index.py
@@ -1,9 +1,19 @@
 msg="Welcome to Python 101: Strings"
 print(msg)
 
-
 # find and replace 
 
 print(msg.find('Python'))
 
 print(msg.replace('Python', 'JavaScript'))
+
+
+# check for 'Python' in msg
+print('Python' in msg)
+
+name='TERRY'
+color = 'RED'
+msg2 = '[' + name + '] loves the color ' + color + '!'
+msg3 = f'[{name}] loves the color {color.lower()}!'
+print(msg2)
+print(msg3)
